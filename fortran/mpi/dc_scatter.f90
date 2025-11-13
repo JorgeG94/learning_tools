e809149d7762368b54a9cf0972a3653623c7cddd
Jorge <jg4@iastate.edu>
2025-11-12 22:00:07 -0600

    this works nicely

--- fortran/mpi/dc_scatter.f90
+++ fortran/mpi/dc_scatter.f90
@@ -1,7 +1,8 @@
-program mpi_saxpy_3d_host
+program mpi_saxpy_3d
   use, intrinsic :: iso_fortran_env, only : real64
   use mpi_f08
   use omp_lib
+  use nvtx
   implicit none
 
   integer :: ierr, nranks, myrank
@@ -13,6 +14,7 @@ program mpi_saxpy_3d_host
   real(real64) :: alpha = 2.0_real64
   real(real64) :: local_sum, global_sum
   real(real64) :: t1, t2
+  real(real64) :: t1_rank, t2_rank
   integer :: i, j, k, r
   integer :: niters, ii
   integer :: narg
@@ -52,9 +54,10 @@ program mpi_saxpy_3d_host
      kstart   = remainder * (base + 1) + (myrank - remainder) * base + 1
   end if
   kend = kstart + nk_local - 1
-
+call nvtxStartRange("Domain decompo")
   ! mpi crap
   if (myrank == 0) then
+    t1_rank = omp_get_wtime()
      allocate(sendcounts(nranks), displs(nranks))
      do r = 0, nranks - 1
         if (r < remainder) then
@@ -68,36 +71,52 @@ program mpi_saxpy_3d_host
      end do
 
      allocate(a_global(dims, dims, dims))
+     !$omp target enter data map(alloc:a_global)
      
      ! can this be initialized on the GPU? 
      do concurrent(k=1:dims, j=1:dims, i=1:dims)
        a_global(i,j,k) = 1.0_real64
      end do
+     t2_rank = omp_get_wtime()
+     print '(A,F18.8)', 'Time rank 0 (s) = ', t2_rank - t1_rank
   end if
+  call nvtxEndRange
 
+  call nvtxStartRange("allocs and scatter")
   ! --- allocate local arrays ---
   allocate(a(dims, dims, max(1, nk_local)))
   allocate(b(dims, dims, max(1, nk_local)))
   allocate(c(dims, dims, max(1, nk_local)))
+  !$omp target enter data map(alloc: a,b,c)
 
   ! --- scatter subdomains ---
+  !$omp target data use_device_addr(a_global, a)
   call MPI_Scatterv(a_global, sendcounts, displs, MPI_REAL8, &
                     a, dims*dims*nk_local, MPI_REAL8, 0, MPI_COMM_WORLD, ierr)
+  !$omp end target data
 
   if (myrank == 0) then
      deallocate(a_global, sendcounts, displs)
+     !$omp target exit data map(delete:a_global)
   end if
+  !!$omp target update to(a)
 
+  call MPI_Barrier(MPI_COMM_WORLD, ierr)
+  t2 = omp_get_wtime()
+  call nvtxEndRange
+  call nvtxStartRange("Main compiutational loop")
+
+  if (myrank == 0) then
+     print '(A,F18.8)', 'Time init (s) = ', t2 - t1
+  end if
 ! initialize things, alloc on the GPU 
+  call MPI_Barrier(MPI_COMM_WORLD, ierr)
+  t1 = omp_get_wtime()
 
-  !$omp target enter data map(alloc: b,c)
-  !$omp target enter data map(to: a)
   do concurrent (k = 1:nk_local, j = 1:dims, i = 1:dims)
     b(i,j,k) = 2.0_real64
     c(i,j,k) = 0.0_real64
   end do
-  b = 2.0_real64
-  c = 0.0_real64
 
   ! perform JAXPY
   do ii = 1, niters
@@ -113,6 +132,8 @@ program mpi_saxpy_3d_host
   end do
   !$omp target update from(local_sum)
   !$omp target exit data map(delete:b,c)
+  call nvtxEndRange()
+  call nvtxStartRange("final reduce and print")
 
   call MPI_Reduce(local_sum, global_sum, 1, MPI_REAL8, MPI_SUM, 0, MPI_COMM_WORLD, ierr)
 
@@ -123,10 +144,11 @@ program mpi_saxpy_3d_host
   if (myrank == 0) then
      print '(A,ES18.8)', 'Global sum = ', global_sum
      print '(A,ES18.8)', 'Expected   = ', 4.0_real64 * dims**3
-     print '(A,ES18.8)', 'Time (s) = ', t2 - t1
+     print '(A,F18.8)', 'Time (s) = ', t2 - t1
   end if
 
+  call nvtxEndRange()
   deallocate(a,b,c)
   call MPI_Finalize(ierr)
-end program mpi_saxpy_3d_host
+end program mpi_saxpy_3d
 
